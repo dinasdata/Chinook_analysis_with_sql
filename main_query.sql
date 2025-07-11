c06debff13690c3461c8c1aabc83280076faaed4
Dina <andrianambinintsoadina@Gmail.com>
2025-07-11 16:01:29 +0300

    Updating the main query

--- main_query.sql
+++ main_query.sql
@@ -33,3 +33,26 @@ group by Year
 select distinct Country, count(I.CustomerId) as total_customer from Chinook.Invoice I join Chinook.Customer C on
 I.CustomerId = C.CustomerId
 group by Country;
+/*Number of Invoices per country*/
+select distinct Country, count(I.CustomerId) as total_customer from Chinook.Invoice I join Chinook.Customer C on
+I.CustomerId = C.CustomerId
+group by Country;
+/*Total of tacks per playlist*/
+select Name, count(p.TrackId) as Total_track from Chinook.PlaylistTrack p join 
+Chinook.Playlist t on p.PlaylistId = t.PlaylistId
+group by Name
+order by Name;
+/* Total sales made by every sale agent*/
+use Chinook;
+select concat(E.FirstName," ",E.LastName) as Name, count(V.InvoiceId) as Total_sales from 
+ Chinook.Invoice V join Chinook.Customer C on V.CustomerId = C.CustomerId join (select * from Chinook.Employee where Title like "%Sales%" ) as E on E.EmployeeId = C.SupportRepId
+group by Name;
+/*total sales per country*/
+select distinct BillingCountry, count(InvoiceId) as total_sales from Chinook.Invoice
+group by BillingCountry
+order by BillingCountry;
+/*Getting  top 10 most purrchased track*/
+select distinct T.Name, count(J.Total) as total_sales from Chinook.Track T 
+join Chinook.InvoiceLine I on T.TrackId = I.TrackId join Chinook.Invoice J on I.InvoiceId= J.InvoiceId
+group by T.Name
+order by total_sales  desc limit 10;
